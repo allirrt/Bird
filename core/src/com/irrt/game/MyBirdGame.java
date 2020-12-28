7c956fcc0ec4eed5ac9b321803e3d92bc0649623
IRRT <zindgg@gmail.com>
2020-12-28 09:37:40 +0300

    Птичка полетела и научилась прыгать и труты вверх, вниз

--- core/src/com/irrt/game/MyBirdGame.java
+++ core/src/com/irrt/game/MyBirdGame.java
@@ -6,7 +6,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyBirdGeme extends ApplicationAdapter {
+import java.util.Random;
+
+public class MyBirdGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
 	Texture[] bird;
@@ -14,8 +16,12 @@ public class MyBirdGeme extends ApplicationAdapter {
 	float flyHeight;
 	float fallingSpeed = 0;//скорость падения
     int gameStateFlag = 0;
+    Texture topTube;
+    Texture bottomTube;
+    int spaceBetweenTube = 500; //величина пространчтва между трубами
+    float tubeShift; //сдвиг труб
+    Random random;
 
-	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -23,13 +29,17 @@ public class MyBirdGeme extends ApplicationAdapter {
 		bird = new Texture[2];
         bird[0] = new Texture("bird_wings_up.png");
         bird[1] = new Texture("bird_wings_down.png");
-
+        topTube = new Texture("top_tube.png");
+        bottomTube = new Texture("bottom_tube.png");
+        random = new Random();
 
         flyHeight =  Gdx.graphics.getHeight()/2 - bird[0].getHeight()/2;//высота полета
 	}
 
 	@Override
 	public void render () {
+        batch.begin();//запускаем игру
+        batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());//рисуем background
 	    if (Gdx.input.justTouched()){ //когда произошло прикосновение
         gameStateFlag = 1;
         }
@@ -37,6 +47,8 @@ public class MyBirdGeme extends ApplicationAdapter {
 
             if (Gdx.input.justTouched()){ //когда произошло прикосновение
                 fallingSpeed = -30;
+               tubeShift = (random.nextFloat() - 0.5f) *
+                       (Gdx.graphics.getHeight() - spaceBetweenTube - 200);//двигаем трубы вверх и вниз рандомно
             }
             if (flyHeight > 0 || fallingSpeed < 0){//чтобы птичка не улетала выше горизонта
                 fallingSpeed++;
@@ -47,16 +59,20 @@ public class MyBirdGeme extends ApplicationAdapter {
                 gameStateFlag = 1;
             }
         }
+        //рисуем трубы
+        batch.draw(topTube,Gdx.graphics.getWidth()/2 - topTube.getWidth()/2, Gdx.graphics.getHeight() / 2 +
+                spaceBetweenTube / 2 + tubeShift);//рисуем верхнюю трубу(координаты)
+        batch.draw(bottomTube,Gdx.graphics.getWidth()/2 - bottomTube.getWidth()/2, Gdx.graphics.getHeight() / 2 -
+                spaceBetweenTube / 2 - bottomTube.getHeight() + tubeShift);//рисуем нижнюю трубу(координаты)
 
       if (birdStateFlag == 0){
           birdStateFlag = 1;
       }else {birdStateFlag= 0;}
 
-   batch.begin();//запускаем игру
-   batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());//рисуем
+
 
    batch.draw(bird[birdStateFlag], Gdx.graphics.getWidth()/2 - bird[birdStateFlag].getWidth()/2,
-           flyHeight);//рисуем
+           flyHeight);//рисуем птичку
    batch.end();
 	}
 	
